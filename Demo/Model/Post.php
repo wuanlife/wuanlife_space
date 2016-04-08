34232e50644cd4cdbc4dde308e6d42b28c8e9a88
ccccy <605246595@qq.com>
2016-04-08 10:16:43 +0800

    add PageCount

--- Demo/Model/Post.php
+++ Demo/Model/Post.php
@@ -5,6 +5,7 @@ class Model_Post extends PhalApi_Model_NotORM {
     public function getIndexPost($page) {
 
         $num=6;
+        $rs[]   = array();
         $sql = 'SELECT pb.title,pd.text,pd.createTime,ub.nickName,gb.name '
              . 'FROM post_detail pd,post_base pb ,group_base gb,user_base ub '
              . 'WHERE pb.id=pd.post_base_id AND pb.user_base_id=ub.id AND pb.group_base_id=gb.id '
@@ -12,7 +13,15 @@ class Model_Post extends PhalApi_Model_NotORM {
              . 'ORDER BY MAX(pd.createTime) DESC '
              . 'LIMIT :start,:num ';
         $params = array(':start' =>($page-1)*$num , ':num' =>$num);
-        return DI()->notorm->user_base->queryAll($sql, $params);
+        $rs = DI()->notorm->user_base->queryAll($sql, $params);
+
+        $sql = 'SELECT ceil(count(*)/:num) AS pageCount '
+             . 'FROM post_base ';
+
+        $params = array(':num' =>$num);
+        $rs[] = DI()->notorm->user_base->queryAll($sql, $params);
+        $rs[] = $page;
+        return $rs;
     }
 
     public function getGroupPost($groupID,$page) {
@@ -25,7 +34,16 @@ class Model_Post extends PhalApi_Model_NotORM {
              . 'ORDER BY MAX(pd.createTime) DESC '
              . 'LIMIT :start,:num ';
         $params = array(':group_id' =>$groupID,':start' =>($page-1)*$num , ':num' =>$num);
-        return DI()->notorm->post_base->queryAll($sql, $params);
+        $rs[] = DI()->notorm->post_base->queryAll($sql, $params);
+
+        $sql = 'SELECT ceil(count(*)/:num) AS pageCount '
+             . 'FROM post_base pb,group_base gb '
+             . 'WHERE pb.id=gb.id AND gb.id=:group_id ';
+
+        $params = array(':group_id' =>$groupID,':num' =>$num);
+        $rs[] = DI()->notorm->user_base->queryAll($sql, $params);
+        $rs[] = $page;
+        return $rs;
     }
 
     public function getMyGroupPost($userID,$page) {
@@ -39,7 +57,16 @@ class Model_Post extends PhalApi_Model_NotORM {
              . 'ORDER BY MAX(pd.createTime) DESC '
               . 'LIMIT :start,:num ';
         $params = array(':user_id' =>$userID,':start' =>($page-1)*$num , ':num' =>$num );
-        return DI()->notorm->post_base->queryAll($sql, $params);
+        $rs[] = DI()->notorm->post_base->queryAll($sql, $params);
+
+        $sql = 'SELECT ceil(count(*)/:num) AS pageCount '
+             . 'FROM post_base pb,group_base gb,group_detail gd '
+             . 'WHERE pb.id=gb.id AND gb.id=gd.group_base_id AND gd.user_base_id=:user_id ';
+
+        $params = array(':user_id' =>$userID,':num' =>$num);
+        $rs[] = DI()->notorm->user_base->queryAll($sql, $params);
+        $rs[] = $page;
+        return $rs;
     }
 
     public function getPostDetail($postID,$page) {
@@ -60,6 +87,13 @@ class Model_Post extends PhalApi_Model_NotORM {
         ->limit(($page-1)*$num,$num)
         ->fetchAll();
 
+        $sql = 'SELECT ceil(count(*)/:num) AS pageCount '
+         . 'FROM post_detail '
+         . 'WHERE floor>1 AND post_base_id=:post_id ';
+
+        $params = array(':post_id' =>$postID,':num' =>$num);
+        $rs[] = DI()->notorm->user_base->queryAll($sql, $params);
+        $rs[] = $page;
         return $rs;
     }
 
