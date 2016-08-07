ac63bdf5bfc3b26a2d644ffce8dd55ceb3eaba7f
Xin <396460741@qq.com>
2016-08-07 17:04:52 +0800

    个人中心路由

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var config = require('../config/config');
+var ua = require('mobile-agent');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    if (req.session.user) {
+        var agent = ua(req.headers['user-agent']);
+        var userid = req.session.user.userID;
+        var page = agent.Mobile ? 'personalMobile' : 'personal';
+        request(config.server + "?service=User.getUserInfo&user_id=" + userid,
+            function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var result = JSON.parse(body);
+                    console.log(result);
+                    if (result.ret == 200 && result.data) {
+                        res.render(page, {
+                            'result': result.data,
+                            'title': '个人中心',
+                            'user': req.session.user
+                        });
+                    } else {
+                        res.render('error', {
+                            'message': result.msg,
+                            error: {
+                                'status': result.ret,
+                                'stack': ''
+                            }
+                        });
+                    }
+                } else {
+                    console.error('user failed:', error);
+                    next(error);
+                }
+            });
+    } else {
+        res.redirect('/login');
+    }
 });
 
 module.exports = router;
