8d35bb4ea4244ee085fc9ebdc41c1350dd6c8cca
Xin <396460741@qq.com>
2016-08-08 17:40:22 +0800

    添加邮箱验证路由

--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,7 @@ router.post('/', function(req, res, next) {
     request.post({
         url: config.server + '?service=User.alterUserInfo',
         formData: {
-            user_id: req.session.user,
+            user_id: req.session.user.userID,
             sex: req.body.sex,
             year:req.body.year,
             month:req.body.month,
@@ -52,7 +52,7 @@ router.post('/', function(req, res, next) {
         }
     }, function optionalCallback(err, httpResponse, body) {
         if (err) {
-            console.error('Login failed:', err);
+            console.error('alter info failed:', err);
             res.header('Content-type', 'application/json');
             res.header('Charset', 'utf8');
             res.send({
@@ -66,4 +66,46 @@ router.post('/', function(req, res, next) {
     });
 });
 
+router.get('/verify', function(req, res, next) {
+    if (req.session.user) {
+        var agent = ua(req.headers['user-agent']);
+        var userid = req.session.user.userID;
+        var page = agent.Mobile ? 'verifyEmailMobile' : 'verifyEmail';
+        var tip = agent.Mobile ? 'tipMobile' : 'tip';
+        request(config.server + "?service=User.GetMailChecked&user_id=" + userid,
+            function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var result = JSON.parse(body);
+                    console.log(result);
+                    if (result.ret == 200 && result.data) {
+                        if (result.data.mailChecked == 1) {
+                            res.render(tip,{
+                                'tip': '您的邮箱已通过验证',
+                                'title': '验证邮箱',
+                                'user': req.session.user
+                            });
+                        } else{
+                            res.render(page,{
+                                'title':'验证邮箱',
+                                'user': req.session.user
+                            });
+                        }
+                    } else {
+                        res.render('error', {
+                            'message': result.msg,
+                            error: {
+                                'status': result.ret,
+                                'stack': ''
+                            }
+                        });
+                    }
+                } else {
+                    console.error('user failed:', error);
+                    next(error);
+                }
+            });
+    } else {
+        res.redirect('/login');
+    }
+});
 module.exports = router;
