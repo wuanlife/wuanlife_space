c777e9e841829f485ef3ee68fda89d85f672abea
Xin <396460741@qq.com>
2016-08-08 10:30:28 +0800

    添加用户信息修改路由

--- routes/users.js
+++ routes/users.js
@@ -40,4 +40,30 @@ router.get('/', function(req, res, next) {
     }
 });
 
+router.post('/', function(req, res, next) {
+    request.post({
+        url: config.server + '?service=User.alterUserInfo',
+        formData: {
+            user_id: req.session.user,
+            sex: req.body.sex,
+            year:req.body.year,
+            month:req.body.month,
+            day:req.body.day
+        }
+    }, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            console.error('Login failed:', err);
+            res.header('Content-type', 'application/json');
+            res.header('Charset', 'utf8');
+            res.send({
+                err: err
+            });
+        }
+        console.log('alter info successful!  Server responded with:', body);
+        res.header('Content-type', 'application/json');
+        res.header('Charset', 'utf8');
+        res.send(JSON.parse(body));
+    });
+});
+
 module.exports = router;
