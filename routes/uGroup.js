107b142d77ed4c9420681431a3ab24f4351474d7
Xin <396460741@qq.com>
2016-09-01 12:21:01 +0800

    Promise

--- routes/uGroup.js
+++ routes/uGroup.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var request = require('request');
 var config = require('../config/config');
 var ua = require('mobile-agent');
+var Promise = require('bluebird');
+Promise.promisifyAll(request);
 
 /* GET users listing. */
 
@@ -40,34 +42,36 @@ router.get('/:userid', function(req, res, next) {
 });
 
 router.get('/:userid/moreGroups', function(req, res, next) {
-    var agent = ua(req.headers['user-agent']);
-    request(config.server + "?service=Group.GetCreate&user_id=" + req.param("userid"),
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body);
-                if (result.ret == 200 && result.msg == "") {
-                    var page = agent.Mobile ? 'moreGroupsMobile' : 'moreGroups';
-                    res.render(page, {
-                        'path':'../../',
-                        result: result.data,
-                        'title':result.data.user_name + "加入的星球",
-                        'userid':req.param("userid"),
-                        'ownerName':result.data.user_name,
-                        'user': req.session.user
-                    });
-                } else {
-					res.render('error', {
-						'message': result.msg,
-						error: {
-							'status': result.ret,
-							'stack': ''
-						}
-					});
-				}
-            } else {
-                console.error('CreateGroups failed:', error);
-                next(error);
+    var agent = ua(req.headers['user-agent']),
+        pn = req.query.page || 1;
+    Promise.all([request.getAsync(config.server + "?service=Group.GetCreate&user_id=" + req.param("userid") + "&page=" + pn),
+        request.getAsync(config.server + "?service=Group.GetJoined&user_id=" + req.param("userid") + "&page=" + pn)]).spread(function(create,joined){
+            var createResult = JSON.parse(create.body);
+            var joinedResult = JSON.parse(joined.body);
+            console.log(createResult);
+            console.log(joinedResult);
+            if (createResult.ret == 200 && joinedResult.ret == 200) {
+                var page = agent.Mobile ? 'moreGroupsMobile' : 'moreGroups';
+                return res.render(page, {
+                    cResult: createResult.data,
+                    jResult: joinedResult.data,
+                    title: createResult.data.user_name + "加入的星球",
+                    userid: req.param("userid"),
+                    ownerName: createResult.data.user_name,
+                    'user': req.session.user
+                });
+            } else{
+                return res.render('error', {
+                    message: createResult.msg || joinedResult.msg,
+                    error: {
+                        'status': 404,
+                        'stack': ''
+                    }
+                });
             }
-        })
+        }).catch(function(error){
+            console.error('moreGroups failed:', error);
+            next(error);
+        });
 });
 module.exports = router;
