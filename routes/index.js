eb283a7beb35cb77bf6c71f3c7b69fb38b00c8b8
Xin <396460741@qq.com>
2016-10-10 13:38:06 +0800

    Catching Exception

--- routes/index.js
+++ routes/index.js
@@ -11,30 +11,28 @@ router.get('/', function(req, res, next) {
 	request(config.server + '?service=Post.GetIndexPost&pn=' + pn, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			//console.log(JSON.parse(body)); // Show the HTML for the Google homepage. 
-			var data = JSON.parse(body);
-			if (data.ret == 200) {
-				var page = agent.Mobile ? 'indexMobile' : 'index';
-				res.render(page, {
-					'path':'',
-					'result': data.data,
-					'title':'首页',
-					'user':req.session.user
-				});
-			} else {
-				res.render('error', {
-					'message': data.msg,
-					error: {
-						'status': data.ret,
-						'stack': ''
-					}
-				});
+			try{
+				var data = JSON.parse(body);
+				if (data.ret == 200) {
+					var page = agent.Mobile ? 'indexMobile' : 'index';
+					res.render(page, {
+						'result': data.data,
+						'title':'首页',
+						'user':req.session.user
+					});
+				} else {
+					res.render('error', {
+						'message': data.msg,
+						error: {
+							'status': data.ret,
+							'stack': ''
+						}
+					});
+				}
+			} catch(e){
+				next(e);
 			}
 		} else {
-			//			res.status(err.status || 500);
-			//			res.render('error', {
-			//				message: response.statusCode + '/n服务器忙，请稍后再试！',
-			//				error: {}
-			//			});
 			next(error);
 		}
 	})
