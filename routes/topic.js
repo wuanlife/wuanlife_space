03376b74592b9979c8adc5e68c02ab379f24f1ff
Xin <396460741@qq.com>
2016-10-19 11:38:11 +0800

    帖子发布及编辑格式设置

--- routes/topic.js
+++ routes/topic.js
@@ -7,153 +7,122 @@ var xss = require('xss');
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-	var agent = ua(req.headers['user-agent']);
-	var userid = (req.session.user) ? req.session.user.userID : null;
-	request(config.server + "?service=Post.GetPostBase&post_id=" + req.param('id') + "&id=" + userid,
-		function(err, response, body) {
-			if (!err && response.statusCode == 200) {
-				var result = JSON.parse(body);
-				//console.log(result);
-				if (result.ret == 200 && result.msg == "") {
-					if (result.data.code == 1) {
-						var page = agent.Mobile ? 'topicMobile' : 'topic';
-						res.render(page, {
-							result: result.data,
-							'title': result.data.title,
-							'user': req.session.user
-						});
-					} else{
-						res.redirect('/group/' + result.data.groupID);
-					}
-				} else {
-					res.render('error', {
-						'message': result.msg || '未加入该私密星球',
-						error: {
-							'status': result.ret,
-							'stack': ''
-						}
-					});
-				}
-			} else{
-				next(err);
-			}
-		});
+    var agent = ua(req.headers['user-agent']);
+    var userid = (req.session.user) ? req.session.user.userID : null;
+    request(config.server + "?service=Post.GetPostBase&post_id=" + req.param('id') + "&id=" + userid,
+        function(err, response, body) {
+            if (!err && response.statusCode == 200) {
+                var result = JSON.parse(body);
+                //console.log(result);
+                if (result.ret == 200 && result.msg == "") {
+                    if (result.data.code == 1) {
+                        var page = agent.Mobile ? 'topicMobile' : 'topic';
+                        res.render(page, {
+                            result: result.data,
+                            'title': result.data.title,
+                            'user': req.session.user
+                        });
+                    } else {
+                        res.redirect('/group/' + result.data.groupID);
+                    }
+                } else {
+                    res.render('error', {
+                        'message': result.msg || '未加入该私密星球',
+                        error: {
+                            'status': result.ret,
+                            'stack': ''
+                        }
+                    });
+                }
+            } else {
+                next(err);
+            }
+        });
 });
 
 
 router.get('/:id/edit', function(req, res, next) {
-	var agent = ua(req.headers['user-agent']);
-	var userid = (req.session.user) ? req.session.user.userID : null;
-	if (agent.Mobile) {
-		res.render('error', {
-			'message': result.msg,
-			error: {
-				'status': 404,
-				'stack': ''
-			}
-		});
-	} else {
-		request(config.server + "?service=Post.GetPostBase&post_id=" + req.param('id') + "&id=" + userid,
-			function(err, response, body) {
-				if (!err && response.statusCode == 200) {
-					var result = JSON.parse(body);
-					//console.log(result);
-					if (result.ret == 200 && result.msg == "" && result.data.editRight == 1) {
-						res.render('postEdit', {
-							'path': '../../',
-							'topicID': req.param('id'),
-							'title': '编辑帖子',
-							'user': req.session.user
-						});
-					} else {
-						res.render('error', {
-							'message': result.msg,
-							error: {
-								'status': 404,
-								'stack': ''
-							}
-						});
-					}
-				} else {
-					next(err);
-				}
-			});
-	}
+    var agent = ua(req.headers['user-agent']);
+    var userid = (req.session.user) ? req.session.user.userID : null;
+    if (agent.Mobile) {
+        res.render('error', {
+            'message': result.msg,
+            error: {
+                'status': 404,
+                'stack': ''
+            }
+        });
+    } else {
+        request(config.server + "?service=Post.GetPostBase&post_id=" + req.param('id') + "&id=" + userid,
+            function(err, response, body) {
+                if (!err && response.statusCode == 200) {
+                    var result = JSON.parse(body);
+                    //console.log(result);
+                    if (result.ret == 200 && result.msg == "" && result.data.editRight == 1) {
+                        res.render('postEdit', {
+                            'path': '../../',
+                            'topicID': req.param('id'),
+                            'title': '编辑帖子',
+                            'user': req.session.user
+                        });
+                    } else {
+                        res.render('error', {
+                            'message': result.msg,
+                            error: {
+                                'status': 404,
+                                'stack': ''
+                            }
+                        });
+                    }
+                } else {
+                    next(err);
+                }
+            });
+    }
 });
 
 router.post('/:id/edit', function(req, res, next) {
-	var userid = (req.session.user) ? req.session.user.userID : null;
-	var html = xss(req.body.text, {
-		onTag: function(tag, html, options) {
-			if (tag == "strike") {
-				return html;
-			}
-		},
-		onTagAttr:function(tag,name,value,isWhiteAttr){
-			if (name == 'style') {
-				return name + '="' + value + '"';
-			}
-		}
-	});
-	request.post({
-		url: config.server + '?service=Post.editPost',
-		formData: {
-			user_id: userid,
-			post_id: req.params.id,
-			title: xss(req.body.title),
-			text: html
-		}
-	}, function optionalCallback(err, httpResponse, body) {
-		if (!err && httpResponse.statusCode == 200) {
-			var result = JSON.parse(body);
-			//console.log(result);
-			if (result.ret == 200 && result.msg == "" ) {
-				res.redirect('/topic/' + req.params.id);
-			} else {
-				res.render('error', {
-					'message': result.msg,
-					error: {
-						'status': 404,
-						'stack': ''
-					}
-				});
-			}
-		} else {
-			next(err);
-		}
-	});
+    var userid = (req.session.user) ? req.session.user.userID : null;
+    var html = xss(req.body.text, {
+        onTag: function(tag, html, options) {
+            if (tag == "strike") {
+                return html;
+            }
+        },
+        onTagAttr: function(tag, name, value, isWhiteAttr) {
+            if (name == 'style') {
+                return name + '="' + value + '"';
+            }
+        }
+    });
+    request.post({
+        url: config.server + '?service=Post.editPost',
+        formData: {
+            user_id: userid,
+            post_id: req.params.id,
+            title: xss(req.body.title),
+            text: html
+        }
+    }, function optionalCallback(err, httpResponse, body) {
+        res.header('Content-type', 'application/json');
+        res.header('Charset', 'utf8');
+        try {
+            if (!err && httpResponse.statusCode == 200) {
+                var result = JSON.parse(body);
+                //console.log(result);
+                return res.send(result);
+            }
+            res.send({
+                ret: 500,
+                msg: '服务器异常'
+            });
+        } catch (e) {
+            res.send({
+                ret: 500,
+                msg: '服务器异常'
+            })
+        }
+    });
 
 });
-
-// router.post('/:id/edit', function(req, res, next) {
-// 	var userid = (req.session.user) ? req.session.user.userID : null;
-// 	var html = xss(req.param('text'),{
-// 		onTag:function(tag,html,options){
-// 			if (tag == "strike") {
-// 				return html;
-// 			}
-// 		}
-// 	});
-// 	request.post({
-// 		url: config.server + '?service=Post.editPost',
-// 		formData: {
-// 			user_id: userid,
-// 			post_id: req.param('id'),
-// 			title: xss(req.param('title')),
-// 			text: html
-// 		}
-// 	}, function optionalCallback(err, httpResponse, body) {
-// 		if (err) {
-// 			console.error('Edit failed:', err);
-// 			next(err);
-// 		}
-// 		var result = JSON.parse(body);
-// 		console.log('edit success:');
-// 		console.log(JSON.parse(body)); 
-// 		res.header('Content-type', 'application/json');
-// 		res.header('Charset', 'utf8');
-// 		res.send(JSON.parse(body));
-// 	});
-
-// });
 module.exports = router;
