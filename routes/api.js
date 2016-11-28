88d5aaf8e953e40d611fea35a0d4bd936f9ac728
haha214 <396460741@qq.com>
2016-11-28 16:54:57 +0800

    change message params

--- routes/api.js
+++ routes/api.js
@@ -123,11 +123,11 @@ router.get('/:method', function(req, res, next) {
                 }
 			});
 			break;
-        case 'getMessageShow':
+        case 'getMessageShow': 
             var pn = req.param('currentpage');
-            var user_id = req.param('userId');
             var status = req.param('status');
-            request(config.server + '?service=User.ShowMessage&user_id=' + user_id + '&pn=' + pn + '&status=' + status, function(error, response, body) {
+            var mtype = req.query.mtype;
+            request(config.server + '?service=User.ShowMessage&user_id=' + userID + '&pn=' + pn + '&status=' + status + '&mtype=' + mtype, function(error, response, body) {
                 res.header('Content-type', 'application/json');
                 res.header('Charset', 'utf8');
                 try{
