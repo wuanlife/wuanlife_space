e9721e071d29780db4a28ee0fead193c3edff196
rizzle <734783679@qq.com>
2016-11-30 19:41:20 +0800

    here bug

--- routes/message.js
+++ routes/message.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
             pn = req.query.page || 1,
             userid = req.session.user.userID,
             status = req.query.status || 1,
-            mtype = 1;
+            mtype = req.query.mtype || 1;
             request(config.server + "?service=User.ShowMessage&user_id=" + userid + "&pn=" + pn + "&status=" + status + "&mtype=" + mtype,
                 function(error, response, body) {
                 if (!error && response.statusCode == 200) {
