5bd3435288a66a047484aa924437d4924dfa704e
rizzle <734783679@qq.com>
2016-12-03 21:08:17 +0800

    debug

--- routes/message.js
+++ routes/message.js
@@ -18,7 +18,6 @@ router.get('/', function(req, res, next) {
                     var result = JSON.parse(body);
                     console.log(result);
                     if (result.ret == 200 && result.msg == "") {
-
                         if (mtype == 3) {
                             var page = agent.Mobile ? 'messageMobile' : 'message';
                             res.render(page, {
@@ -34,8 +33,6 @@ router.get('/', function(req, res, next) {
                                 'user': req.session.user
                             });                            
                         }
-
-
                     } else {
                         res.render('error', {
                             'message': result.msg,
