f22f7b612d4d0f82f080aed95956f3f9b28ec179
haha214 <396460741@qq.com>
2017-03-02 17:11:38 +0800

    add less

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var path = require('path');
 var reload = browserSync.reload;
+var less  = require('gulp-less');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
 
 //path 定义
 var basedir = './';
@@ -9,7 +12,9 @@ var publicdir = './public/';
 var filepath = {
     'css' : path.join(publicdir,'stylesheets/*.css'),
     'js' : path.join(publicdir,'javascripts/**/*.js'),
-    'ejs': path.join(basedir,'views/**/*.ejs')
+    'ejs': path.join(basedir,'views/**/*.ejs'),
+    'less': path.join(publicdir,'stylesheets/*.less'),
+    'style': path.join(publicdir,'stylesheets')
 }
 //监听任务
 gulp.task('watch',function(){
@@ -25,5 +30,18 @@ gulp.task('watch',function(){
     // gulp.watch('./public/**').on('change', reload);
 });
 
+//编译less
+ gulp.task('compileLess',function(){
+    gulp.src(filepath.less)
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(less())
+        .pipe(gulp.dest(filepath.style));
+ });
+
+//监听less变化
+gulp.task('watchLess',function(){
+    gulp.watch(filepath.less,['compileLess']);
+});
+
 //默认任务
-gulp.task('default',['watch']);
+gulp.task('default',['watch','watchLess']);
