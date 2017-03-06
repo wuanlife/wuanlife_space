c93b22d025ebc40e27b7c4b27ca3513d0111ef6a
haha214 <396460741@qq.com>
2017-03-06 10:16:17 +0800

    change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var path = require('path');
-var reload = browserSync.reload;
-var less  = require('gulp-less');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    path = require('path'),
+    reload = browserSync.reload,
+    less  = require('gulp-less'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    minifyCss = require("gulp-minify-css"),
+    concat = require("gulp-concat"),
+    watch = require('gulp-watch')
 
 //path 定义
 var basedir = './';
 var publicdir = './public/';
 var filepath = {
-    'css' : path.join(publicdir,'stylesheets/*.css'),
+    'css' : path.join(publicdir,'stylesheets/dist/*.css'),
     'js' : path.join(publicdir,'javascripts/**/*.js'),
     'ejs': path.join(basedir,'views/**/*.ejs'),
-    'less': path.join(publicdir,'stylesheets/*.less'),
-    'style': path.join(publicdir,'stylesheets')
+    'less': path.join(publicdir,'stylesheets/src/less/!(global*).less'),
+    'gLess': path.join(publicdir,'stylesheets/src/less/global*.less'),
+    'pcCss': path.join(publicdir,'stylesheets/src/css/*PC.css'),
+    'mCss' : path.join(publicdir,'stylesheets/src/css/*Mobile.css'),
+    'srcStyle': path.join(publicdir,'stylesheets/src/css'),
+    'style': path.join(publicdir,'stylesheets/dist')
 }
 //监听任务
 gulp.task('watch',function(){
@@ -33,15 +40,41 @@ gulp.task('watch',function(){
 //编译less
  gulp.task('compileLess',function(){
     gulp.src(filepath.less)
+        .pipe(watch(filepath.less))
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(less())
+        .pipe(gulp.dest(filepath.srcStyle))
+ });
+
+//编译globalLess
+gulp.task('gLess',function(){
+    gulp.src(filepath.gLess)
+        .pipe(watch(filepath.gLess))
         .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
         .pipe(less())
         .pipe(gulp.dest(filepath.style));
- });
 
-//监听less变化
+});
+
+//合并PCcss文件
+gulp.task('concatPCCss',function(){
+    gulp.src(filepath.pcCss)
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest(filepath.style));
+});
+
+//合并Mobilecss文件
+gulp.task('concatMCss',function(){
+    gulp.src(filepath.mCss)
+        .pipe(concat('styleMobile.css'))
+        .pipe(gulp.dest(filepath.style));
+});
+
+//监听less变化并合并css
 gulp.task('watchLess',function(){
-    gulp.watch(filepath.less,['compileLess']);
+    gulp.watch(filepath.pcCss,['concatPCCss']);
+    gulp.watch(filepath.mCss,['concatMCss']);
 });
 
 //默认任务
-gulp.task('default',['watch','watchLess']);
+gulp.task('default',['watch','compileLess','gLess','watchLess']);
