23f4424730cd00567759d285beab4f8791854ff6
lwyj123 <443474713@qq.com>
2017-03-14 00:07:30 -0700

    add mymessage-verify

--- routes/myMessage.js
+++ routes/myMessage.js
@@ -29,7 +29,30 @@ router.get('/post', function(req, res, next) {
         next(e);
     }
 });
-
+router.get('/planet', function(req, res, next) {
+    var agent = ua(req.headers['user-agent']);
+    try{
+        var page = agent.Mobile ? 'mymessageMobile-planet' : 'mymessage-planet';
+        res.render(page, {
+            'result': null,
+            'user': null
+        });
+    } catch(e){
+        next(e);
+    }
+});
+router.get('/verify', function(req, res, next) {
+    var agent = ua(req.headers['user-agent']);
+    try{
+        var page = agent.Mobile ? 'mymessageMobile-verify' : 'mymessage-verify';
+        res.render(page, {
+            'result': null,
+            'user': null
+        });
+    } catch(e){
+        next(e);
+    }
+});
 
 
 
