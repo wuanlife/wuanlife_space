2ed511a02f01d18d695d48265ac7e00b9e983d3e
lwyj123 <443474713@qq.com>
2017-04-23 21:34:14 +0800

    update postdetails

--- routes/postdetails.js
+++ routes/postdetails.js
@@ -39,7 +39,38 @@ router.post('/:postid/reply', function(req, res, next) {
         }
     )
 });
-
+router.post('/:postid', function(req, res, next) {
+    if(!req.session.user) {
+        res.send({
+            ret: 403,
+            msg: '未登录'
+        });
+        return;
+    }
+    request(`${config.server}post/get_post_base?user_id=${req.session.user.user_id}&post_id=${req.params.postid}`,
+        function(error, httpResponse, body) {
+            if (!error && httpResponse.statusCode == 200) {
+                console.log('get_index_post success!');
+                return res.send(JSON.parse(body));
+            } else {
+                console.log('get_index_post error!  Server responded with:', body);
+                try {
+                    console.error('get_index_post failed:', error.toString());
+                    res.send({
+                        ret: 500,
+                        msg:'服务器异常'
+                    });
+                } catch(error) {
+                    console.error('catch get_index_post exception:', error.toString());
+                    res.send({
+                        ret: 500,
+                        msg:'服务器异常'
+                    });
+                }
+            }
+        }
+    )
+});
 router.get('/:postid', function(req, res, next) {
 	/*
     if(!req.session.user) {
