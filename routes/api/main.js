5d1286a5a23db8aa7730bedf29378d6e5079eccb
lwyj123 <443474713@qq.com>
2017-05-21 15:12:15 +0800

    fix header

--- routes/api/main.js
+++ routes/api/main.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var ua = require('mobile-agent');
+var config = require('../../config/config');
 
 router.use(function(req, res, next) {
   console.log('Something is happening.');
@@ -9,33 +12,20 @@ router.use(function(req, res, next) {
 router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our rest video api!' });  
 });
-
-
-
-router.route('/videos')
-
-  .post(function(req, res) {
-
-    var video = new Video();
-    video.title = req.body.title;
-
-    video.save(function(err) {
-  if (err)
-    res.send(err);
-
-  res.json({ message: 'Video criado!' });
+router.post('/checkNewInfo', function(req, res) {
+    request(`${config.server}user/check_new_info?user_id=${req.session.user.user_id}`,
+        function(error, httpResponse, body) {
+            if (!error && httpResponse.statusCode == 200) {
+                console.log('aplly success!');
+                res.send(JSON.parse(body));
+            } else {
+                res.send({
+                    ret: 500,
+                    msg:'服务器异常'
+                });
+            }
+        }
+    ); 
 });
-
-
-  })
-
-  .get(function(req, res) {
-    Video.find(function(err, videos) {
-      if (err)
-        res.send(err);
-
-      res.json(videos);
-    });
-  });
 
 module.exports.router = router;
