49d20623f150821c440ce51eca20db94afad8f6e
lwyj123 <443474713@qq.com>
2017-05-30 20:15:27 +0800

    fix some bug in issues

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,26 @@ var config = require('../../config/config');
 router.get('/', function(req, res) {
   res.json({ message: 'this is groups' });  
 });
+router.route('/:postid')
+    .put(function(req, res, next) {
+        // set Top
+        if(req.body.setTop) {
+            request(`${config.server}post/delete_post_reply?user_id=${req.session.user.user_id}&post_id=${req.params.postid}&floor=${req.params.repliyid}`,
+                function(error, httpResponse, body) {
+                    if (!error && httpResponse.statusCode == 200) {
+                        console.log('delete success!');
+                        res.send(JSON.parse(body));
+                    } else {
+                        res.send({
+                            ret: 500,
+                            msg:'服务器异常'
+                        });
+                    }
+                }
+            );              
+        }
+      
+    })
 
 router.route('/:postid/replies/:repliyid')
     .delete(function(req, res, next) {
