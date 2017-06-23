655ec8cc8487334cf685f904facb006d798b184e
Breakinferno <1972952841@qq.com>
2017-06-23 22:03:17 +0800

    update error

--- routes/myCollections.js
+++ routes/myCollections.js
@@ -20,10 +20,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/:id', function(req, res, next) {
-    request(`${config.server}post/collect_post?post_id=${req.params.postid}&user_id=${req.session.user.user_id}`,
+    console.log(`${config.server}post/collect_post?post_id=${req.params.id}&user_id=${req.session.user.user_id}`);
+    request(`${config.server}post/collect_post?post_id=${req.params.id}&user_id=${req.session.user.user_id}`,
         function(error, httpResponse, body) {
             if (!error && httpResponse.statusCode == 200) {
                 console.log('cancel collection success!');
+
+                console.log(JSON.parse(body).msg);
                 return res.send(JSON.parse(body));
             } else {
                 console.log('cancel collection error!  Server responded with:', body);
