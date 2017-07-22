9113fe84ac24247a2349278984e20a298b947a25
lwio <443474713@qq.com>
2017-07-22 23:18:00 +0800

    initial

---
+++ src/views/index/visitor/index.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="index-visitor-container">
+      <section>
+      
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="myplanet" name="index-myplanet">
+            <div class="index-tabcontent" v-loading="loading">
+              asdasdasdasdasdas
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="myplanet" name="index-newtopic">
+            <div class="index-tabcontent" v-loading="loading2">
+              newtopic
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </section>
+      <aside>
+        <header>
+          <h1>发现星球</h1>
+        </header>
+        <div class="aside-container">
+          
+        </div>
+      </aside>
+    </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'index-visitor',
+    data() {
+      return {
+        activeName: 'index-myplanet',
+        loading: false,
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'user',
+        'access_token',
+      ])
+    },
+    mounted() {
+      this.loading = true;
+      this.loading2 = true;
+      // simulate ajax loading
+      setTimeout(() => {
+        this.loading = false;
+      }, 4000)
+      setTimeout(() => {
+        this.loading2 = false;
+      }, 6000)
+
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .index-visitor-container {
+    margin: auto;
+    width: 750px;
+  }
+  .index-tabcontent {
+    min-height: 200px;
+  }
+</style>
---
+++ src/views/layout/Navbar.vue
@@ -0,0 +1,105 @@
+<template>
+  <el-menu class="navbar" mode="horizontal">
+    <levelbar></levelbar>
+    <tabs-view></tabs-view>
+    <error-log v-if="log.length>0" class="errLog-container" :logsList="log"></error-log>
+    <el-dropdown class="avatar-container" trigger="click">
+      <div class="avatar-wrapper">
+        <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+        <i class="el-icon-caret-bottom"></i>
+      </div>
+      <el-dropdown-menu class="user-dropdown" slot="dropdown">
+        <router-link class='inlineBlock' to="/">
+          <el-dropdown-item>
+            午安网
+          </el-dropdown-item>
+        </router-link>
+        <a target='_blank' href="https://github.com/PanJiaChen/vue-element-admin/">
+          <el-dropdown-item>
+            项目地址
+          </el-dropdown-item>
+        </a>
+        <el-dropdown-item divided><span @click="logout" style="display:block;">退出登录</span></el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </el-menu>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import Levelbar from './Levelbar';
+  import TabsView from './TabsView';
+  import ErrorLog from 'components/ErrLog';
+  import errLogStore from 'store/errLog';
+
+  export default {
+    components: {
+      Levelbar,
+      TabsView,
+      ErrorLog,
+    },
+    data() {
+      return {
+        log: errLogStore.state.errLog
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'name',
+        'avatar'
+      ])
+    },
+    methods: {
+      logout() {
+        this.$store.dispatch('LogOut').then(() => {
+          location.reload();// 为了重新实例化vue-router对象 避免bug
+        });
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .navbar {
+    height: 50px;
+    line-height: 50px;
+    border-radius: 0px !important;
+    background-color: #5677fc;
+    .errLog-container {
+      display: inline-block;
+      position: absolute;
+      right: 150px;
+    }
+    .screenfull {
+      position: absolute;
+      right: 90px;
+      top: 16px;
+      color: red;
+    }
+    .avatar-container {
+      height: 50px;
+      display: inline-block;
+      position: absolute;
+      right: 35px;
+      .avatar-wrapper {
+        cursor: pointer;
+        margin-top: 5px;
+        position: relative;
+        .user-avatar {
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+        .el-icon-caret-bottom {
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
+    }
+  }
+</style>
+
+
+
