92cd1e788d3937820054609a3cfa55741cb5f60d
tim760255458 <qq760255458@gmail.com>
2017-07-28 15:41:02 +0800

    Create index.vue

---
+++ src/views/collection/index.vue
@@ -0,0 +1,163 @@
+<template>
+    <div class="collection-container">
+      <section>     
+        <header>
+            我的收藏
+        </header>
+        <div class="index-tabcontent" v-loading="loading2">
+          <ul class="index-cards">
+            <li class="index-card">
+              <div class="index-card-content">
+                <h1>Ubuntu开荒笔记</h1>
+                <div class="preview-html">
+                  安装后。终端操作：sudo apt updatesudo apt upgrade重启，执行上述update命令一次，然后执行：sudo apt install ***常用的软件安装后。终端操作：sudo apt updatesudo apt upgrade重启，执行上述update命令一次，然后执行：sudo apt install ***常用的软件...
+                </div>
+                <div class="preview-imgs">
+                  <img src="#">
+                  <img src="#">
+                  <img src="#">
+                </div>
+              </div>
+              <footer>
+                <span>鬼扯天地</span>
+                <div>
+                  <span>收藏于</span>
+                  <time>2017-02-21 22:41</time>
+                </div>
+              </footer>
+            </li>
+            <li class="index-card">
+              <div class="index-card-content">
+                <h1>Ubuntu开荒笔记</h1>
+                <div class="preview-html">
+                  安装后。终端操作：sudo apt updatesudo apt upgrade重启，执行上述update命令一次，然后执行：sudo apt install ***常用的软件安装后。终端操作：sudo apt updatesudo apt upgrade重启，执行上述update命令一次，然后执行：sudo apt install ***常用的软件...
+                </div>
+                <div class="preview-imgs"></div>
+              </div>
+              <footer>
+                <span>鬼扯天地</span>
+                <div>
+                  <span>收藏于</span>
+                  <time>2017-02-21 22:41</time>
+                </div>
+              </footer>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'collection-container',
+    data() {
+      return {
+        activeName: 'index-myplanet',
+        loading: false,
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'user',
+        'access_token',
+      ])
+    },
+    mounted() {
+      this.loading = true;
+      this.loading2 = true;
+      // simulate ajax loading
+      setTimeout(() => {
+        this.loading = false;
+      }, 4000)
+      setTimeout(() => {
+        this.loading2 = false;
+      }, 6000)
+
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .collection-container {
+    display: flex;
+    justify-content: space-between;
+    margin: auto;
+    max-width: 590px;
+    min-width: 590px;
+    @media screen and (max-width: 900px) {
+      justify-content: center;
+    }
+    section {
+      min-width: 0;
+      flex: 0 0 590px;
+      header {
+        margin: 15px 0 20px 0;
+        font-family:PingFangHK-Medium;
+        font-size:18px;
+        color:#5677fc;
+      }
+    }
+  }
+  .index-tabcontent {
+    min-height: 200px;
+    margin-top: 5px;
+  }
+  // post card style    
+  .index-cards { 
+    .index-card {   
+      padding: 10px 16px 12px 16px;   
+      background-color: #ffffff;
+      border-radius: 8px;  
+      &:not(:first-child) {
+        margin-top: 8px;
+      }  
+      footer {    
+        display: flex;    
+        align-items: center;
+        font-size:12px;   
+        color:#999999;
+        position: relative;
+        div {
+          position: absolute;
+          right: 0;
+        }
+        span {    
+          &:not(:first-child) {   
+            margin-left: 5px;   
+          }
+        } 
+      }
+      div.index-card-content {
+        margin-bottom: 12px;
+        h1 {
+          margin-bottom: 6px;
+          color: #003585;
+          font-family:PingFangHK-Semibold;
+          font-size:16px;
+        }
+        div.preview-html {
+          margin-bottom: 14px;
+          word-break: break-all;
+          font-size:14px;
+          color:#666666;
+          letter-spacing:0;
+          text-align:justify;
+        }
+        div.preview-imgs {
+          display: flex;
+          justify-content: space-between;
+          img {
+            width: 174px;
+            height: 174px;
+            background:#d8d8d8;
+            border-radius:4px;
+            align-self: center;
+          }
+        }
+      }
+    }   
+  }
+</style>
