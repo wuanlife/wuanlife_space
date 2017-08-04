ba597a4b3e2f1d75da46610844baa7d5405437ba
lwio <443474713@qq.com>
2017-08-04 18:09:16 +0800

    change dev api server

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"development"',
-    BASE_API: '"http://13.75.120.29:800"',
+    BASE_API: '"http://104.199.245.21:800/"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 }
