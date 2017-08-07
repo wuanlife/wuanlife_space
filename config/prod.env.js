167d6be8052c3b347d0b8b3f7b2fd440004f1cc0
lwio <443474713@qq.com>
2017-08-07 13:25:28 +0800

    update api

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"https://api-prod"',
+    BASE_API: '"http://104.199.245.21:800/"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 };
