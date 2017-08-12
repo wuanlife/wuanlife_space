fe1a47ce532ac8e4e2f9cfa859f509db8b915d87
lwio <443474713@qq.com>
2017-08-12 22:31:17 +0800

    fix message image bug

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -13,7 +13,7 @@ const service = axios.create({
 service.interceptors.request.use(config => {
   // Do something before request is sent
   if (store.getters.user.token) {
-    config.headers['Access-Token'] = store.getters.user.token; // 让每个请求携带token--['X-Token']为自定义key 请根据实际情况自行修改
+    config.headers['Access-Token'] = store.getters.user.token;
   }
   return config;
 }, error => {
