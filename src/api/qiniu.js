a885a227ca597b0b51219cf72716f03a6a6445f2
lwio <443474713@qq.com>
2017-08-12 18:32:47 +0800

    add qiniu demo

--- src/api/qiniu.js
+++ src/api/qiniu.js
@@ -1,5 +1,9 @@
 import fetch from 'utils/fetch';
 
+// use a imageServer or just back-end server, uploadToken should not be generated in front-end 
+
+
+// OPTIMIZE: make it configurable
 export function getToken() {
   return fetch({
     url: '/qiniu/upload/token', // 假地址 自行替换
