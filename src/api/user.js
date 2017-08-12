02ab5e0e8fce316f2f8e0feb32bc35ce3ce1cc49
lwio <443474713@qq.com>
2017-08-13 00:32:44 +0800

    start reuse uploader

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,17 @@
 import fetch from 'utils/fetch';
+// get user details
 export function getUser(id) {
   return fetch({
     url: `/users/${id}`,
     method: 'get',
+  });
+}
+
+// change user details
+export function putUser(id, params) {
+  return fetch({
+    url: `/users/${id}`,
+    method: 'put',
+    data: params,
   });
 }
