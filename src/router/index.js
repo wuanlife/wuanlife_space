cc89831e33914d48559c4ca7f60a3a558faa9ed4
lwio <443474713@qq.com>
2017-08-20 23:07:08 +0800

    change api server in production

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const _import = require('./_import_' + process.env.NODE_ENV);
 // in development env not use Lazy Loading,because Lazy Loading large page will cause webpack hot update too slow.so only in production use Lazy Loading
 
 /* layout */
-import Layout from 'views/layout/Layout';
+const Layout = _import('layout/Layout');
 
 /* login */
 const Login = _import('login/index');
@@ -22,8 +22,7 @@ const GroupCreate = _import('group/create/index');
 const Inform = _import('inform/index');
 const InviteCode = _import('invitecode/index');
 
-
-import Collection from 'views/collection'
+const Collection = _import('collection/index');
 
 import PersonalData from 'views/personalData'
 
