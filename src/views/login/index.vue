9a241103af52a18188469fa26d03652210b55e7e
TyrQueen <156832264@qq.com>
2018-01-11 20:26:04 +0800

    完成基本样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,48 @@
 <template>
-    <div class="login-container">
-      <section>     
-        <header>登录</header>
-        <div class="form-content" v-loading="loading">
-          <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-width="100px" class="demo-ruleForm" @keyup.enter.native="submitForm('loginForm')">
-            
+  <div class="login-container view-container">
+    <section>
+
+      <header>登录</header>
+
+      <div class="people-circle">
+        <icon-svg icon-class="peopleCircle_white" class="peopleCircle_white-icon"></icon-svg>
+      </div>
+
+      <div class="form-content" v-loading="loading">
+
+
+
+        <el-form :model="loginForm" :rules="loginRules" ref="loginForm" class="demo-ruleForm" @keyup.enter.native="submitForm('loginForm')">
+
+          <div class="email-input">
+
             <icon-svg icon-class="email" class="youxiang-icon"></icon-svg>
-            <el-form-item label="" prop="email" class="form-inputy">
+
+            <el-form-item class="form-inputy" prop="email">
               <el-input v-model="loginForm.email" placeholder="输入邮箱"></el-input>
             </el-form-item>
-            
+
+          </div>
+
+          <div class="psw-input">
+
             <icon-svg icon-class="code" class="mima-icon"></icon-svg>
-            <el-form-item label="" prop="password" class="form-inputy">
+
+            <el-form-item prop="password" class="form-inputy">
               <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="输入密码"></el-input>
             </el-form-item>
-            
-            <el-form-item label-width="100px" class="form-btny">
-              <el-button type="primary" :loading="loading" :disabled="loading" @click="submitForm('loginForm')">登录</el-button>
-            </el-form-item>
-          </el-form>
 
-          <!--<a href="#/signup/" id="register">注册账号</a>
-          <a href="#/findpassword/" id="findpassword">找回密码</a>-->
-          
-        </div>
-      </section>
-    </div>
+          </div>
+
+          <el-form-item class="form-btny">
+            <el-button type="primary" :loading="loading" :disabled="loading" @click="submitForm('loginForm')">登录</el-button>
+          </el-form-item>
+
+        </el-form>
+
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -60,7 +77,8 @@
         },
         loginRules: {
           email: [
-            { validator: validateEmail, trigger: 'blur' }
+            { validator: validateEmail, trigger: 'blur' },
+            { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
           ],
           password: [
             { validator: validatePass, trigger: 'blur' }
@@ -108,38 +126,62 @@
     display: flex;
     justify-content: space-between;
     margin: auto;
-    max-width: 660px;
+    max-width: 828px;
     min-width: 380px;
-    section {
-      flex: 1;
-      header {
-        margin: 15px 0 20px 0;
+    padding-top: 100px;
 
-        font-family:PingFangHK-Medium;
-        font-size:18px;
-        color:#5677fc;
+    section {
+
+      flex: 1;
+
+      header {
+        margin: 20px auto 0;
+        font-family: MicrosoftYaHei-Bold;
+        font-size: 32px;
+        color: #5677fc;
       }
-      div.form-content {
-        width: 100%;
-        background:#ffffff;
-        border-radius:4px;
-        width:660px;
-        height:500px;
-        padding-top:40px;
-        #register{
-          font-family:PingFangHK-Medium;
-          font-size:14px;
-          color:#5677fc;
-          margin-left:144px;
+
+      .people-circle {
+        width: 78px;
+        height: 157px;
+        float: right;
+        margin-top: -56px;
+        margin-right: 51px;
+        background-color: #5677fc;
+        text-align: center;
+
+        clip-path: polygon( 0 0, 78px 0, 78px 150px, 50px 127px, 0 157px);
+
+        .peopleCircle_white-icon {
+          width: 53px;
+          height: 54px;
+          color: white;
+          margin-top: 35px;
         }
-        #findpassword{
-          font-family:PingFangHK-Medium;
-          font-size:14px;
-          color:#5677fc;
-          margin-left:260px;
+      }
+
+      .form-content {
+        width: 100%;
+        background: #ffffff;
+        border-radius: 4px;
+        width: 660px;
+        height: 625px;
+        padding-top: 86px;
+        margin: 0 auto;
+
+        .form-btny {
+          text-align: center;
+
+          button {
+            width: 458px;
+            /*height: 72px;*/
+            background-color: #5677fc;
+            border-radius: 4px;
+            font-size: 24px;
+            font-family: MicrosoftYaHeiLight;
+          }
         }
       }
     }
-
   }
 </style>
