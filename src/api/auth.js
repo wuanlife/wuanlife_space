a5816c4bb2a98918dfea954a3143c5192ff4e7e7
TyrQueen <156832264@qq.com>
2018-01-13 14:09:52 +0800

    去掉注册所需邀请码

--- src/api/auth.js
+++ src/api/auth.js
@@ -16,8 +16,7 @@ export function signup(regObj) {
   const data = {
     name: regObj.nickname,
     mail: regObj.email,
-    password: regObj.password,
-    code: regObj.inviteword
+    password: regObj.password
   };
   return fetch({
     url: '/users',
