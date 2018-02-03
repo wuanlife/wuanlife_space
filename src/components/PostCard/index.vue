87dc3898dd5192712457c0178dc7fe79e81db924
tim760255458 <qq760255458@gmail.com>
2018-02-03 07:28:38 -0600

    样式完成，去掉 store (#157)
    * postCard 样式修改完成
    
    * 修改样式，postCard 请求缩略图片
    
    * 去掉 store

--- src/components/PostCard/index.vue
+++ src/components/PostCard/index.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="post-card" >
     <header>
-      <img :src="post.author.avatar_url === 'default_url' ? 'http://7xlx4u.com1.z0.glb.clouddn.com/o_1aqt96pink2kvkhj13111r15tr7.jpg?imageView2/1/w/100/h/100' : post.author.avatar_url">
+      <img :src="post.author.avatar_url === 'default_url' ? 'http://7xlx4u.com1.z0.glb.clouddn.com/o_1aqt96pink2kvkhj13111r15tr7.jpg?imageView2/1/w/32/h/32' : post.author.avatar_url">
       <span class="clickable" @click="$router.push({path: `/myspace/${post.author.id}`})">{{ post.author.name }}</span>
       <time>{{ post.create_at | formatTime }}</time>
     </header>
@@ -10,7 +10,7 @@
       <div class="preview-html" v-html="content">
       </div>
       <div class="preview-imgs">
-        <img v-for="(img,index) of post.image_urls" :key="index" :src="img">
+        <img v-for="(img,index) of post.image_urls" :key="index" :src="img + '?imageView2/1/w/132/h/132'">
       </div>
     </div>
     <footer>
@@ -110,22 +110,22 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" scoped>
   .post-card {
-    padding: 38px 26px 0px 26px;
-    width: 714px;
+    padding: 24px 17px 0px 17px;
+    width: 448px;
     background-color: #ffffff;
     border-radius: 4px;
     &:not(:first-child) {
-      margin-top: 20px;
+      margin-top: 12px;
     }
     &:last-child {
-      margin-bottom: 108px;
+      margin-bottom: 78px;
     }
     header {
       display: flex;
       align-items: center;
-      margin-bottom: 27px;
+      margin-bottom: 17px;
       font-size:12px;
-      color:#999999;
+      color:#434343;
       & > .clickable {
         transition: all 0.2s ease-in-out;
         &:hover {
@@ -133,13 +133,14 @@ export default {
         }
       }
       img {
-        width: 50px;
-        height: 50px;
+        width: 32px;
+        height: 32px;
         border-radius: 100%;
-        margin-right: 24px;
+        margin-right: 14px;
+        background-color: #aaaaaa;
       }
       span {
-        font-size: 24px;
+        font-size: 15px;
         color: #333333;
         &:not(:first-child) {
           margin-left: 5px;
@@ -147,7 +148,7 @@ export default {
       }
       time {
         margin-left: 12px;
-        font-size: 18px;
+        font-size: 11px;
         color: #999999;
         flex-grow: 1;
         text-align: right;
@@ -159,14 +160,14 @@ export default {
         display: inline-block;
         position: relative;
         cursor: pointer;
-        margin-bottom: 19px;
+        margin-bottom: 11px;
         max-width: 100%;
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
 
-        color: #333333;
-        font-size:24px;
+        color: #434343;
+        font-size:15px;
         // hover animation
         &::after {
           content: '';
@@ -180,6 +181,7 @@ export default {
           background: #2e5897;
         }
         &:hover {
+          color: #5677fc;
           &::after {
             transform: scaleX(1);
           }
@@ -187,7 +189,7 @@ export default {
       }
       div.preview-html {
         word-break: break-all;
-        font-size:18px;
+        font-size:11px;
         color:#444444;
         letter-spacing:0;
         text-align:justify;
@@ -198,26 +200,27 @@ export default {
       }
       div.preview-imgs {
         display: flex;
-        margin-top: 33px;
+        margin-top: 20px;
         img {
-          margin-right: 15px;
-          width: 210px;
-          height: 210px;
+          margin-right: 9px;
+          width: 132px;
+          height: 132px;
+          background-color: #f8f9fa;
         }
 
       }
     }
     footer {
-      margin-top: 40px;
-      margin-left: -26px;
-      margin-right: -26px;
-      border-top: 1px solid #dcdcdc;
-      padding: 14px 0;
+      margin-top: 24px;
+      margin-left: -17px;
+      margin-right: -17px;
+      border-top: 2px solid #dcdcdc;
+      padding: 10px 0 9px 0;
       ul {
         display: flex;
         justify-content: space-around;
-        font-size: 20px;
-        height: 30px;
+        font-size: 12px;
+        height: 19px;
         align-items: center;
         li {
           transition: all 0.2s ease-in-out;
@@ -227,8 +230,8 @@ export default {
           color: #666666;
           border-right: 2px solid #dcdcdc;
           .avatar-icon{
-            margin-right: 20px;
-            font-size: 24px;
+            margin-right: 13px;
+            font-size: 12px;
           }
           &:last-child{
             border: 0;
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
 import { login, signup } from 'api/auth'
 import { putUser } from 'api/user'
-import { storeWithExpiration } from 'utils'
+// import { storeWithExpiration } from 'utils'
 
-const loadUser = () => {
-  const user = storeWithExpiration.get('user')
-  if (user && user.id) {
-    return user
-  }
-}
+// const loadUser = () => {
+//   const user = storeWithExpiration.get('user')
+//   if (user && user.id) {
+//     return user
+//   }
+// }
 
 const user = {
-  state: {
-    ...loadUser()
-  },
+  state: {},
   mutations: {
     SET_USER: (state, userInfo) => {
       for (const key in userInfo) {
@@ -32,29 +30,29 @@ const user = {
     async Login ({ commit }, params) {
       const userWithToken = await login(params)
       commit('SET_USER', userWithToken)
-      storeWithExpiration.set('user', userWithToken)
+      // storeWithExpiration.set('user', userWithToken)
       return userWithToken
     },
     // for later one-use token, Logout should in actions
     Logout ({ commit }) {
-      commit('LOGOUT_USER')
-      storeWithExpiration.set('user', {})
+      commit('CLEAR_USER')
+      // storeWithExpiration.set('user', {})
     },
     // 注册
     async Signup ({ commit }, params) {
       const userWithToken = await signup(params)
       commit('SET_USER', userWithToken)
-      storeWithExpiration.set('user', userWithToken)
+      // storeWithExpiration.set('user', userWithToken)
       return userWithToken
     },
     async PutUser ({ commit, state }, params) {
       const backMessage = await putUser(params)
       const user = Object.create(null)
       Object.assign(user, state, params)
-      storeWithExpiration.set('user', {
-        ...user,
-        'Access-Token': loadUser()['Access-Token']
-      }, 86400000)
+      // storeWithExpiration.set('user', {
+      //   ...user,
+      //   'Access-Token': ''
+      // }, 86400000)
       commit('SET_USER', user)
       return backMessage
       // return new Promise((resolve, reject) => {
