7a3287a24e8069eae6235742971c22ac222f8ffa
18566246732 <18566246732@163.com>
2018-03-03 19:55:20 +0800

    creat vue-websocket plugins

--- src/plugin/vue-websocket.js
+++ src/plugin/vue-websocket.js
@@ -3,8 +3,22 @@ class WuanWebsocket {
     this._url = url
     this._user = {}
     this._ws = new Websocket(url)
+    this._mesQueue = {}
+    this._channelMsgQueue = {}
+    this.connection()
+  }
+  connection() {
     this._ws.addEventListener('open', function (event) {
-      console.log('ws link')
+      console.log('connection opened !')
+    })
+    this._ws.addEventListener('message', function (event) {
+      processMsg(event)
+    })
+    this._ws.addEventListener('error', function (event) {
+      processError(event)
+    })
+    this._ws.addEventListener('close', function (event) {
+      console.log('connection closed !')
     })
   }
   /**
@@ -13,23 +27,51 @@ class WuanWebsocket {
    * @param  {Function} callback 回调函数
    */
   onByType (msgType, callback) {
-
+    if(!this._mesQueue[msgType])
+    this._mesQueue[msgType] = [];
+    this._mesQueue[msgType].push(callback);
   }
-
+  /**
+   * 触发某种type的消息
+   * @param  {String} msgType 消息类型
+   * @param  {Array} arg 回调函数的参数数组
+   */
+  emitByType (msgType,...arg) {
+    let len = this._mesQueue[msgType].length
+    if (len != 0) {
+    	this._mesQueue[msgType].forEach(function (item) {
+    		item(...arg)
+    	})
+    }
+  }
   /**
    * 监听某个channel的消息
    * @param  {String} channel 消息类型
    * @param  {Function} callback 回调函数
    */
   onByChannel (channel, callback) {
-
+    if(!this._channelMsgQueue[channel])
+    this._channelMsgQueue[channel] = [];
+    this._channelMsgQueue[channel].push(callback);
   }
-
+  /**
+   * 触发某个channel的消息
+   * @param  {String} channel 消息类型
+   * @param  {Array} arg 回调函数的参数数组
+   */
+  emitByChannel (channel,...arg) {
+    let len = this._channelMsgQueue[channel].length
+    if (len != 0) {
+      this._channelMsgQueue[channel].forEach(function (item) {
+        item(...arg)
+      })
+    }
+  }
   /**
    * @param  {Object} message 需要send的message对象，自动添加uuid和source等
    */
   send (message) {
-
+    this._ws.send(JSON.stringify(message))
   }
 
   /**
@@ -44,7 +86,33 @@ class WuanWebsocket {
    * 断开websocket连接
    */
   terminate () {
-
+    this._ws.close()
+  }
+  /**
+   * 处理服务器响应的消息
+   */
+  processMsg (e) {
+    let data = JSON.parse(e.data)
+    switch (data.type){
+    	case 'chat-subscribe-ok':
+    	//do something here ...
+    		break;
+    	case 'chat-send-ok':
+      //do something here ...
+        break;
+      case 'chat-receive':
+      //do something here ...
+        break;
+    	default:
+    		break;
+    }
+  }
+  /**
+   * 处理错误消息
+   */
+  processError (e) {
+    let data = JSON.parse(e.data)
+    //do something here ...
   }
 }
 
@@ -59,9 +127,7 @@ export default {
       options
     }
     const ws = new WuanWebsocket(`${options.url}`)
-
     Vue.prototype.$ws = ws
-
     Vue.mixin({
       beforeCreate: () => {},
 
